64b0cbd84cccf49d097ff085467823b79e8e3fdd
Pouya Barari.G <b_pooya@yahoo.com>
2022-05-31 18:51:39 +0430

    make first category and products

---
+++ src/js/Storage.js
@@ -0,0 +1,50 @@
+export default class Storage {
+  static getAllCategories() {
+    const savedCategories = JSON.parse(localStorage.getItem("category")) || [];
+    const sortedCategories = savedCategories.sort((a, b) => {
+      return new Date(a.createdAt) > new Date(b.createdAt) ? -1 : 1;
+    });
+    return sortedCategories;
+  }
+  static saveCategory(categoryToSave) {
+    const savedCategories = Storage.getAllCategories();
+    // edit => ... save
+    // new => ... save
+    const existedItem = savedCategories.find((c) => c.id === categoryToSave.id);
+    if (existedItem) {
+      // edit
+      existedItem.title = categoryToSave.title;
+      existedItem.description = categoryToSave.description;
+    } else {
+      // new
+      categoryToSave.id = new Date().getTime();
+      categoryToSave.createdAt = new Date().toISOString();
+      savedCategories.push(categoryToSave);
+    }
+    localStorage.setItem("category", JSON.stringify(savedCategories));
+  }
+  static getAllProducts() {
+    const savedProduct = JSON.parse(localStorage.getItem("products")) || [];
+    return savedProduct.sort((a, b) => {
+      return new Date(a.createdAt) > new Date(b.createdAt) ? -1 : 1;
+    });
+  }
+  static saveProducts(productToSave) {
+    const savedProducts = Storage.getAllProducts();
+    // edit => ... save
+    // new => ... save
+    const existedItem = savedProducts.find((c) => c.id === productToSave.id);
+    if (existedItem) {
+      // edit
+      existedItem.title = productToSave.title;
+      existedItem.quantity = productToSave.quantity;
+      existedItem.category = productToSave.category;
+    } else {
+      // new
+      productToSave.id = new Date().getTime();
+      productToSave.createdAt = new Date().toISOString();
+      savedProducts.push(productToSave);
+    }
+    localStorage.setItem("products", JSON.stringify(savedProducts));
+  }
+}
